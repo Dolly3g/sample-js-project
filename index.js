ba039c514841fe6c3862a8733d5f29b1033c75c5
Dollyg <dollyg@thoughtworks.com>
2016-09-11 14:28:15 +0530

    Added authentication of the user.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var printSuccessMessage = function(){
-	alert("Login Succesful! Welcome");
+var credentials = [{"username": "Bob", "password": "123"}]
+
+var isUserValid = function(username, password){
+	return credentials.some(function(user){
+		if(user.username==username && user.password==password)
+			return true;
+		else
+			return false;
+	})
+}
+
+var validateUser = function(){
+	var username = $('#username')[0].value;
+	var password = $('#password')[0].value;
+
+	if(isUserValid(username, password))
+		alert("Success!!");
+	else
+		alert("Invalid User!")
 }
 
 var onPageLoad = function(){
-	$("#login").click(printSuccessMessage);
+	$("#login").click(validateUser);
 }
 
 $(onPageLoad);
